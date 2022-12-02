74470ba7e7c6289de20bcf00a5e12e6020ac21c6
DMITRIY <DMITRIY@MacBook-Pro-2.local>
2022-12-02 12:32:53 +0600

    Change contract

--- blockchain/contracts/Airdrop.sol
+++ blockchain/contracts/Airdrop.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Airdrop is Ownable, Pausable {
 	IERC20 private _TOKEN;
-	mapping (address => bool) public successAirdrops;
+	mapping (address => bool) private successAirdrops;
 	uint public amountAirdrop;
 	address private _walletWithTokens;
 
@@ -22,7 +22,7 @@ contract Airdrop is Ownable, Pausable {
     // To distribute tokens, it is necessary to transfer the required number of tokens to the contract address
     // Any address gets an airdrop
 	function getAirdrop() external whenNotPaused {
-		require(amountAirdrop != 0, "Airdrop is not started!");
+		require(address(_TOKEN) != address(0), "Airdrop is not started!");
 		require (!successAirdrops[msg.sender], "Your address already received airdrop!");
 		require(getBalance() >= amountAirdrop, "Insufficient balance");
         successAirdrops[msg.sender] = true;
@@ -32,7 +32,7 @@ contract Airdrop is Ownable, Pausable {
 
 	// Before start airdrop necessary approve amount of tokens
 	function getAirdropWithApprove() external whenNotPaused {
-		require(amountAirdrop != 0, "Airdrop is not started!");
+		require(address(_TOKEN) != address(0), "Airdrop is not started!");
 		require (!successAirdrops[msg.sender], "Your address already received airdrop!");
 		require(_TOKEN.allowance(_walletWithTokens, address(this)) >= amountAirdrop, "Unfortunely, token is not approved!");
         successAirdrops[msg.sender] = true;
@@ -41,25 +41,27 @@ contract Airdrop is Ownable, Pausable {
 	}
 
     // Changing the token
-    function setTokenWalletAddrAmount(address _newToken, address _wallet, uint _value) external onlyOwner {
-        require(_newToken != address(0), "Address not be zero!");
-		require(_wallet != address(0), "Address not be zero!");
-        _TOKEN = IERC20(_newToken);
-		_walletWithTokens = _wallet;
+  	function setTokenWalletAddrAmount(address _newToken, address _wallet, uint _value) external onlyOwner {
+        setToken(_newToken);
+		setWallet(_wallet);
 		setAmountAirdrop(_value);
 		emit SetNewToken(address(_TOKEN), _newToken, _wallet, _value);
     }
 
 	// Changing the amount of airdrop
-	function setAmountAirdrop(uint _amount) public onlyOwner {
-        require(_amount != amountAirdrop && _amount != 0, "Change value");
+	function setAmountAirdrop(uint _amount) internal onlyOwner {
+        require(_amount != 0, "Change value");
 		amountAirdrop = _amount;
 	}
 
-	// Removes the address from the list of addresses that received airdrop
-	function cancelSuccessAirdrop(address _droper) external onlyOwner {
-		require(successAirdrops[_droper], "This address not received airdrop!");
-		successAirdrops[_droper] = false;
+	function setWallet(address _newWallet) internal onlyOwner {
+		require(_newWallet != address(0), "Address not be zero!");
+		_walletWithTokens = _newWallet;
+	}
+
+	function setToken(address _newToken) internal onlyOwner {
+		require(_newToken != address(0), "Address not be zero!");
+		_TOKEN = IERC20(_newToken);
 	}
 
 	// Checking the balance of tokens on the contract
